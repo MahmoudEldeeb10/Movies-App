f57036d9bac13a3c5654ef0c9847d85fb0be630e
Mahmoud Elsayed Eldeeb <https://github.com/MahmoudEldeeb10>
2026-04-02 23:30:07 +0200

    edit movies card

--- lib/features/movies/presentation/view/widgets/movie_card.dart
+++ lib/features/movies/presentation/view/widgets/movie_card.dart
@@ -1,42 +1,56 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:movies_app/constants.dart';
+import 'package:movies_app/features/movie_details/presentation/view/movies_details_view.dart';
 
 class MovieCard extends StatelessWidget {
   const MovieCard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(12),
-      child: Stack(
-        children: [
-          CachedNetworkImage(
-            imageUrl: "https://dummyimage.com/300x450/000/fff&text=No",
-            height: double.infinity,
-            width: double.infinity,
-            fit: BoxFit.cover,
-          ),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const MoviesDetailsView()),
+        );
+      },
 
-          Positioned(
-            bottom: 8,
-            left: 0,
-            right: 0,
-            child: Container(
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                // color: Colors.transparent,
-              ),
-              child: Text(
-                'title',
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-                textAlign: TextAlign.center,
-                style: AppTextStyles.title.copyWith(color: Colors.white),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(12),
+        child: Stack(
+          children: [
+            CachedNetworkImage(
+              imageUrl: "https://dummyimage.com/300x450/000/fff&text=No",
+              height: double.infinity,
+              width: double.infinity,
+              fit: BoxFit.cover,
+
+              placeholder: (context, url) =>
+                  const Center(child: CircularProgressIndicator()),
+              errorWidget: (context, url, error) => const Icon(Icons.error),
+            ),
+
+            Positioned(
+              bottom: 8,
+              left: 0,
+              right: 0,
+              child: Container(
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  // color: Colors.transparent,
+                ),
+                child: Text(
+                  'title',
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  textAlign: TextAlign.center,
+                  style: AppTextStyles.title.copyWith(color: Colors.white),
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
