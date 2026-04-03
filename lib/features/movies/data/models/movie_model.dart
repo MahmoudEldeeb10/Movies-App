93db2dae1537a7a94a1ae0bc024bb91ea00624c7
Mahmoud Elsayed Eldeeb <https://github.com/MahmoudEldeeb10>
2026-04-03 15:52:52 +0200

    edit movie model

--- lib/features/movies/data/models/movie_model.dart
+++ lib/features/movies/data/models/movie_model.dart
@@ -1,4 +1,6 @@
 class MovieModel {
+  final int id; // ← زود دي
+
   final String title;
   final String subTitle;
   final String image;
@@ -11,14 +13,17 @@ class MovieModel {
     required this.image,
     required this.rating,
     required this.overview,
+    required this.id,
   });
 
   factory MovieModel.fromJson(Map<String, dynamic> json) {
     return MovieModel(
+      id: json['id'] ?? 0,
+
       title: json['title'] ?? '',
       subTitle: json['subTitle'] ?? '',
       image: json['poster_path'] ?? '',
-      rating: (json['vote_average'] ?? 0).todouble(),
+      rating: (json['vote_average'] ?? 0).toDouble(),
       overview: json['overview'] ?? '',
     );
   }
